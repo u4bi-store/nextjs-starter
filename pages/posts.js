51a2e9dcc916dd2635a25b56cb5f452272529a1c
myungjaeyu <rootcode10@gmail.com>
2018-12-06 18:08:31 +0900

    Added app container with unstated.

--- pages/posts.js
+++ pages/posts.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'
+import { Subscribe } from 'unstated'
+
+import { AppContainer } from '../providers/containers'
 
 export default class extends Component {
-  static getInitialProps ({ query: { id } }) {
-    return { postId: id }
-  }
 
-  render () {
-    return <div>
-      <h1>My blog post #{this.props.postId}</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </p>
-    </div>
-  }
+    static getInitialProps ({ query: { id } }) {
+        return { postId: id }
+    }
+
+    render () {
+        return(
+            <Subscribe to={ [ AppContainer ]}>
+                {
+                    (app) => {
+
+                        console.log(app.state)
+
+                        return(
+                            <div>
+                                <h1>My { app.state.title } #{this.props.postId}</h1>
+                            </div>
+                        )
+
+                    }
+                }
+            </Subscribe>
+        )
+    }
+
 }
