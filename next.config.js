d5da4a38acf2232fea887bc33fc456af65bf1b34
myungjaeyu <rootcode10@gmail.com>
2018-12-07 16:58:28 +0900

    Added next-optimized-images and next-compose-plugins to package.json

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const withPlugins = require('next-compose-plugins')
 const withCss = require('@zeit/next-css')
+const withOptimizedImages = require('next-optimized-images')
 
 if (typeof require !== 'undefined') require.extensions['.css'] = (file) => {}
 
-module.exports = withCss()
+module.exports = withPlugins([
+  [withOptimizedImages],
+  [withCss]
+]);
