035be45aa66cc23f54dd302892187e0d15fcada2
myungjaeyu <rootcode10@gmail.com>
2018-12-08 10:33:51 +0900

    Added styled-components.

--- pages/posts.js
+++ pages/posts.js
@@ -3,6 +3,10 @@ import Link from 'next/link'
 
 import './posts.scss'
 
+import styled from 'styled-components'
+
+const Title = styled.h1` color: blue; font-size: 50px;`
+
 export default class extends Component {
 
     static getInitialProps ({ query: { id } }) {
@@ -13,6 +17,8 @@ export default class extends Component {
         return(
             <div className='Posts'>
                 <h1>My blog post #{this.props.postId}</h1>
+
+                <Title>Styled components</Title>
                                     
                 <ul>
                     <li><Link href='/posts/2' as='/posts/2'><a>post #2</a></Link></li>
